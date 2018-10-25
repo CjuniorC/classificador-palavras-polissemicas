332a1b93ef20033ac57e52756c11e98d890838f0
Junior Silva <cjunior.network@gmail.com>
2018-10-25 19:18:38 -0300

    Testing

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
-from templates.Script import Script
-from templates.Service import Service
-from nltk.corpus import stopwords
-from templates.Tokenizer import Tokenizer
+# from flask_cors import CORS
+# from templates.Script import Script
+# from templates.Service import Service
+# from nltk.corpus import stopwords
+# from templates.Tokenizer import Tokenizer
 
 
-frute = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/frute')
-clothes = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/clothes')
-tagger = Script.execute(frute, clothes);
+# frute = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/frute')
+# clothes = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/clothes')
+# tagger = Script.execute(frute, clothes);
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify(Tokenizer().tokenizer_phrase(sentence.split(' '), tagger))
+    # return jsonify(Tokenizer().tokenizer_phrase(sentence.split(' '), tagger))
+    return 'OK'
     
 
 # CORS(app)
