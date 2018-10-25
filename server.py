ee4a73b587260c299622537cbad92dbcba0113ea
Junior Silva <cjunior.network@gmail.com>
2018-10-25 15:55:16 -0300

    Teste

--- server.py
+++ server.py
@@ -5,20 +5,24 @@ from Service import Service
 from nltk.corpus import stopwords
 
 
-frute = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/frute')
-clothes = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/clothes')
+# frute = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/frute')
+# clothes = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/clothes')
 
 
 app = Flask(__name__)
 
-CORS(app)
-@app.route('/<sentence>', methods=['GET'])
-def classify(sentence):
-    sentence = ' '.join(word for word in sentence.split(' ') if word not in stopwords.words('portuguese'))
-    tagged_sentence = Script.execute(sentence, frute, clothes);
-    return jsonify(tagged_sentence)
+@app.route('/')
+def index():
+    return 'Ok!'
+
+# CORS(app)
+# @app.route('/<sentence>', methods=['GET'])
+# def classify(sentence):
+#     # sentence = ' '.join(word for word in sentence.split(' ') if word not in stopwords.words('portuguese'))
+#     # tagged_sentence = Script.execute(sentence, frute, clothes);
+#     # return jsonify(tagged_sentence)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 
