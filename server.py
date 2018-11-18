ca39faa544956f1a08a5e9f7a77149e78cd215c0
Junior Silva <cjunior.network@gmail.com>
2018-11-18 09:50:20 -0200

    Implementação final

--- server.py
+++ server.py
@@ -1,30 +1,63 @@
 from flask import Flask, jsonify
-# from flask_cors import CORS
-# from templates.Script import Script
-# from templates.Service import Service
-# from nltk.corpus import stopwords
-# from templates.Tokenizer import Tokenizer
-
-
-# frute = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/frute')
-# clothes = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/clothes')
-# tagger = Script.execute(frute, clothes);
-
+from flask_cors import CORS
+from templates.Script import Script
+from templates.Service import Service
+from nltk.corpus import stopwords
+from templates.Tagger import Tagger
+from templates.Corpus import Corpus
+from templates.Tokenizer import Tokenizer
+from templates.Normalizer import Normalizer
+import nltk
+from  unidecode import unidecode
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # return jsonify(Tokenizer().tokenizer_phrase(sentence.split(' '), tagger))
-    return 'OK'
-    
+tagger = ''
+frute = ''
+clothes = ''
+corpus = ''
 
-# CORS(app)
-# @app.route('/<sentence>', methods=['GET'])
-# def classify(sentence):
-#     # sentence = ' '.join(word for word in sentence.split(' ') if word not in stopwords.words('portuguese'))
-#     # tagged_sentence = Script.execute(frute, clothes);
-#     # return jsonify(tagged_sentence)
+CORS(app)
+@app.route('/tag/<sentence>', methods=['GET'])
+def index(sentence):
+    global tagger
+    sentence = unidecode(sentence)
+    sentence = ' '.join(word for word in sentence.split(' ') if word not in stopwords.words('portuguese'))
+    return jsonify(Tokenizer().tokenizer_phrase(sentence.split(' '), tagger))
+
+@app.route('/setup')
+def setup():
+    global tagger
+    global corpus
+    frute = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/frute')
+    clothes = Service.get_corpus('https://classificador-18ac0.firebaseapp.com/clothes')
+    obj = Script.execute(frute, clothes)
+    tagger = obj['tagger']
+    corpus = obj['corpus']
+    return 'ok'
+
+CORS(app)
+@app.route('/metrics/words', methods=['GET'])
+def words():
+    global tagger
+    global corpus
+    total = sum(len(sentence) for sentence in corpus)
+    return jsonify(total)
+
+CORS(app)
+@app.route('/metrics/accuracy', methods=['GET'])
+def accuracy():
+    global tagger
+    global corpus
+    return jsonify(nltk.BigramTagger.evaluate(tagger, corpus));
+   
+
+CORS(app)
+@app.route('/metrics/sentences', methods=['GET'])
+def sentences():
+    global tagger
+    global corpus
+    return jsonify(len(corpus) * 2)
 
 if __name__ == '__main__':
     app.run()
--- templates/Script.py
+++ templates/Script.py
@@ -2,20 +2,29 @@ from templates.Tagger import Tagger
 from templates.Corpus import Corpus
 from templates.Tokenizer import Tokenizer
 from templates.Normalizer import Normalizer
+import math
+from unidecode import unidecode
 
 class Script:
 
 
     @staticmethod
-    def execute(sentence, frute, clothes):
+    def execute(frute, clothes):
         corpus = Tagger().tagger_training_corpus()
         t = Tokenizer().generate_tokenizer(corpus)
+        frute = unidecode(frute)
         n_corpus = Normalizer().break_corpus(frute)
         n_corpus = Corpus().generate_train_base(n_corpus, t, 'manga', 'mangas')
         n_corpus = Tagger().tagger(n_corpus, 'fruta')
+        clothes = unidecode(clothes)
         n_corpus2 = Normalizer().break_corpus(clothes)
         n_corpus2 = Corpus().generate_train_base(n_corpus2, t, 'manga', 'mangas')
         n_corpus2 = Tagger().tagger(n_corpus2, 'roupa')
-        n_corpus = Corpus().join_corpus(n_corpus, n_corpus2)
-        t2 = Tokenizer().generate_tokenizer(n_corpus)
-        return t2
+        train_corpus = Corpus().join_corpus(n_corpus[:math.trunc(len(n_corpus)/2)], n_corpus2[:math.trunc(len(n_corpus)/2)])
+        test_corpus = Corpus().join_corpus(n_corpus[math.trunc(len(n_corpus)/2):], n_corpus2[math.trunc(len(n_corpus)/2):])
+        t2 = Tokenizer().generate_tokenizer(train_corpus)
+        return {'tagger':t2, 'corpus':test_corpus}
+
+
+
+    
